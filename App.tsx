4b9d4bbfac83274ca1388518f0c790dfa92c1c28
lucas <lucasabrts@icloud.com>
2024-08-23 13:11:47 -0300

    feat: to be continued...

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {Text} from './src/components/Text/Text';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Text>AMO REACT NATIVE</Text>
+      <Text preset="headingLarge" style={{color: 'red'}}>
+        AMO REACT NATIVE
+      </Text>
     </SafeAreaView>
   );
 }
