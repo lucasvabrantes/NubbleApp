a66d156cdebdf3cc7307a34010b0c09f5a9f6ef6
lucas <118850665+lucasvabrantes@users.noreply.github.com>
2024-08-26 11:40:14 -0300

    feat: ...

--- App.tsx
+++ App.tsx
@@ -3,17 +3,21 @@ import {SafeAreaView, View} from 'react-native';
 
 import {Text} from './src/components/Text/Text';
 import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <View style={{padding: 10}}>
-        <Text italic bold preset="headingLarge">
-          Lucas Abrantes
-        </Text>
-        <Button title="Entrar"></Button>
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{padding: 10}}>
+          <Text italic bold preset="headingLarge">
+            Lucas Abrantes
+          </Text>
+          <Button title="Entrar"></Button>
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
